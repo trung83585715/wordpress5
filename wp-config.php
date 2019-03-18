ad0b6d1c4d1e9e580973b053fa3afde496d6aa09
trung83585715 <48148864+trung83585715@users.noreply.github.com>
2019-03-18 11:37:43 +0700

    Update and rename wp-config-sample.php to wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'wp_db' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'my_admin_passwd' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         'p*+C_Sy&@jPyv+x961C#LM!7SAm+G-lZl@i> lCpB<X2M0$Xi}mt>RXn;+^]=s&6');
+define('SECURE_AUTH_KEY',  '}pDXTWS3CKG2;|aQSV95=qEI/Oi[C&OToxp{2hlOCmw;IBKJ%)JEGi{bm1l&wn?V');
+define('LOGGED_IN_KEY',    '}4nbg1lI[um/HoWrJ1(W}ld&Y:S<v?p7ok#M~88,W[N]98VJ}EX0&,W+-d;n>!4^');
+define('NONCE_KEY',        'Mu(rp1<Q0x%U [V3l0wM+4;/2ju+)$!^WgYsXOv+/l$7k=|r8)yg`Uyjby@tLRV#');
+define('AUTH_SALT',        '`XR0%u^/ym}Lk3/WjVNfLJ|SIoRu;C9gJ-v={4Ux}@G-3rH>q_1`]U`!]Y+qMOF]');
+define('SECURE_AUTH_SALT', '>H1y{Z+NMBkk9E`f|Ov(OKXg^$_70w3u-||:fvXV-hlDr,W5QV t#>c0eVK`YG8 ');
+define('LOGGED_IN_SALT',   'Bl[XE{Qy!ovn:>>N4dn9_dXeHiwnF_~G7,Q|zt0{*0f9E*{<{DH~TzXAky#kGv|0');
+define('NONCE_SALT',       '5j@,/y`aMsW}_>kV|k<{tH6 uSi4j!9AMyNrG^ h8}x-x-lD3}^t3Q+-&0B#XIAx');
 
 /**#@-*/
 
